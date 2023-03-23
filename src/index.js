2d9de7d68c767d4a528f2223cf80f085a35595ff
Inda Smith <inda.k.smith@gmail.com>
2023-03-23 16:26:56 -0400

    took a bit of time, but was able to make the list.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+
 document.addEventListener("DOMContentLoaded", () => {
+  const taskDescription = document.getElementById('new-task-description')
   // your code here
 });
+
+function addTask(e) {
+  
+  let li = document.createElement('li');
+  let taskList = document.getElementById('tasks');
+  taskList.appendChild(li);
+  li.textContent = e.target["new-task-description"].value;
+  e.preventDefault();
+}
+
+function addToList() {
+  let form = document.getElementById('create-task-form');
+  form.addEventListener('submit', addTask)
+}
+
+addToList()
